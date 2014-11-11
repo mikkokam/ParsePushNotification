250df4d55e15987051fdf737e9dacaf212c4eabd
Bonerchick <jerry82422@hotmail.com>
2014-11-12 01:30:22 +0800

    add initialize function

--- www/parsePushNotification.js
+++ www/parsePushNotification.js
@@ -1,6 +1,8 @@
 var parsePushNotification = {
 	initialize: function(param, successCallback, errorCallback) {
 		cordova.exec(successCallback, errorCallback, 'ParsePushNotification', 'subscribe',[{
+			App_ID: param.App_ID,
+			Client_Key: param.Client_Key
 		}]);
 	},
     subscribe: function(param, successCallback, errorCallback) {
