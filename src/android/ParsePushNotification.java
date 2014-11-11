0c6f9fd12e9fbd7b8c9585a79c411289b46cf958
Bonerchick <jerry82422@hotmail.com>
2014-11-12 00:43:08 +0800

    add parse sdk

--- src/android/ParsePushNotification.java
+++ src/android/ParsePushNotification.java
@@ -8,18 +8,19 @@ import org.json.JSONException;
 import android.util.Log;
 
 public class ParsePushNotification extends CordovaPlugin {
+	public static final String ACTION_INITIALIZE = "initialize";
     public static final String ACTION_SUBSCRIBE = "subscribe";
     
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         try {
-            if (ACTION_SUBSCRIBE.equals(action)) { 
-               JSONObject arg_object = args.getJSONObject(0);
-			   String channel = arg_object.getString("channel");
-				Log.i(channel, channel);
-               callbackContext.success();
-               return true;
-            }
+            if (ACTION_INITIALIZE.equals(action)) { 
+				this.initialize(callbackContext, args);
+				return true;
+            } else if (ACTION_SUBSCRIBE.equals(action)) {
+				this.subscribe(callbackContext, args);
+				return true;
+			}
             callbackContext.error("Invalid action");
             return false;
         } catch(Exception e) {
@@ -27,5 +28,36 @@ public class ParsePushNotification extends CordovaPlugin {
             callbackContext.error(e.getMessage());
             return false;
         } 
+    }
+	
+	private void initialize(final CallbackContext callbackContext, final JSONArray args) {
+        cordova.getThreadPool().execute(new Runnable() {
+            public void run() {
+                try {
+					JSONObject arg_object = args.getJSONObject(0);
+                    String appId = args.getString(0);
+                    String clientKey = args.getString(1);
+                    Parse.initialize(cordova.getActivity(), appId, clientKey);
+                    callbackContext.success();
+                } catch (JSONException e) {
+                    callbackContext.error("JSONException");
+                }
+            }
+        });
+    }
+	
+	private void subscribe(final CallbackContext callbackContext, final JSONArray args) {
+        cordova.getThreadPool().execute(new Runnable() {
+            public void run() {
+                try {               
+					JSONObject arg_object = args.getJSONObject(0);
+			  		String channel = arg_object.getString("channel");
+					Log.i(channel, channel);
+                    callbackContext.success();
+                } catch (JSONException e) {
+                    callbackContext.error("JSONException");
+                }
+            }
+        });
     }
 }
--- www/parsePushNotification.js
+++ www/parsePushNotification.js
@@ -1,4 +1,8 @@
 var parsePushNotification = {
+	initialize: function(param, successCallback, errorCallback) {
+		cordova.exec(successCallback, errorCallback, 'ParsePushNotification', 'subscribe',[{
+		}]);
+	},
     subscribe: function(param, successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback, 'ParsePushNotification', 'subscribe',[{
 			'channel': param.channel
