472e71e0fe99068464976d69bc6f0409bdfa2ade
Bonerchick <jerry82422@hotmail.com>
2014-11-13 21:03:15 +0800

    activity

--- src/android/ParsePushNotificationReceiver.java
+++ src/android/ParsePushNotificationReceiver.java
@@ -1,17 +1,45 @@
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import com.parse.ParsePushBroadcastReceiver;
 
+import android.app.Activity;
+import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.util.Log;
 
 public class ParsePushNotificationReceiver extends ParsePushBroadcastReceiver {
 
     @Override
-    public void onPushOpen(Context context, Intent intent) {
-        Log.e("Push", "Clicked");
-        Intent i = new Intent(context, CordovaApp.class);
-        i.putExtras(intent.getExtras());
-        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        context.startActivity(i);
+    protected void onPushOpen(Context context, Intent intent) {
+
+        String uriString = null;
+        try {
+            JSONObject pushData = new JSONObject(intent.getStringExtra("com.parse.Data"));
+            uriString = pushData.optString("uri");
+        } catch (JSONException e) {
+            Log.v("com.parse.ParsePushReceiver", "Unexpected JSONException when receiving push data: ", e);
+        }
+        Class<? extends Activity> cls = getActivity(context, intent);
+        Intent activityIntent;
+        if (uriString != null && !uriString.isEmpty()) {
+            activityIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(uriString));
+        } else {
+            activityIntent = new Intent(context, CordovaApp.class);
+        }
+        activityIntent.putExtras(intent.getExtras());
+        if (Build.VERSION.SDK_INT >= 16) {
+            TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+            stackBuilder.addParentStack(cls);
+            stackBuilder.addNextIntent(activityIntent);
+            stackBuilder.startActivities();
+        } else {
+            activityIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            activityIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            context.startActivity(activityIntent);
+        }
     }
 }
