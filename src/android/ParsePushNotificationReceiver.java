06cb9cde9471ff8a8c69195639d2261a2b514a19
Bonerchick <jerry82422@hotmail.com>
2014-11-13 20:56:21 +0800

    activity

---
+++ src/android/ParsePushNotificationReceiver.java
@@ -0,0 +1,17 @@
+import com.parse.ParsePushBroadcastReceiver;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class ParsePushNotificationReceiver extends ParsePushBroadcastReceiver {
+
+    @Override
+    public void onPushOpen(Context context, Intent intent) {
+        Log.e("Push", "Clicked");
+        Intent i = new Intent(context, CordovaApp.class);
+        i.putExtras(intent.getExtras());
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(i);
+    }
+}
