59da3c24da3be9516fd0d6525a2853a242cde765
Mikko Kamarainen <mikko.kamarainen@gmail.com>
2014-12-15 18:12:24 +0200

    tabs

--- www/parsePushNotification.js
+++ www/parsePushNotification.js
@@ -1,23 +1,25 @@
 var parsePushNotification = {
+	
     initialize: function(param, successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback, 'ParsePushNotification', 'initialize',[{
             App_ID: param.App_ID,
             Client_Key: param.Client_Key
         }]);
     },
-        subscribe: function(param, successCallback, errorCallback) {
-            cordova.exec(successCallback, errorCallback, 'ParsePushNotification', 'subscribe',[{
-            'channel': param.channel
-        }]);
-        },
-    
-        getInstallationId: function(successCallback, errorCallback) {
-            cordova.exec(successCallback, errorCallback, 'ParsePushNotification', 'getInstallationId',[]);
-        },
 
-        getInstallationObjectId: function(successCallback, errorCallback) {
-            cordova.exec(successCallback, errorCallback, 'ParsePushNotification', 'getInstallationObjectId', []);
-        }
+    subscribe: function(param, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, 'ParsePushNotification', 'subscribe',[{
+        'channel': param.channel
+    }]);
+    },
+
+    getInstallationId: function(successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, 'ParsePushNotification', 'getInstallationId',[]);
+    },
+
+    getInstallationObjectId: function(successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, 'ParsePushNotification', 'getInstallationObjectId', []);
+    }
 };
 
 module.exports = parsePushNotification;
