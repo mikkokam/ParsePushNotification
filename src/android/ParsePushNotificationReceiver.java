cb8d9da87df388e15916595742cf9f8c948c65ba
Mikko Kamarainen <mikko.kamarainen@gmail.com>
2014-12-15 18:13:16 +0200

    tabs

--- src/android/ParsePushNotificationReceiver.java
+++ src/android/ParsePushNotificationReceiver.java
@@ -9,18 +9,18 @@ public class ParsePushNotificationReceiver extends ParsePushBroadcastReceiver {
 
     @Override
     public void onPushOpen(Context context, Intent intent) {
-		String packageName = context.getPackageName();
-		Intent launchIntent = context.getPackageManager().getLaunchIntentForPackage(packageName);
-		String className = launchIntent.getComponent().getClassName();
-		try {
-			Intent i;
-			i = new Intent(context, Class.forName(className));
-	        i.putExtras(intent.getExtras());
-	        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-	        context.startActivity(i);
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+        String packageName = context.getPackageName();
+        Intent launchIntent = context.getPackageManager().getLaunchIntentForPackage(packageName);
+        String className = launchIntent.getComponent().getClassName();
+        try {
+            Intent i;
+            i = new Intent(context, Class.forName(className));
+            i.putExtras(intent.getExtras());
+            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(i);
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }
