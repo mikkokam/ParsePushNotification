74685ca73f694787981d56d7af2ddfdefe9d3276
Mikko Kämäräinen <mikko@infinity.fi>
2014-12-15 17:50:35 +0200

    Update parsePushNotification.js

--- www/parsePushNotification.js
+++ www/parsePushNotification.js
@@ -5,33 +5,19 @@ var parsePushNotification = {
 			Client_Key: param.Client_Key
 		}]);
 	},
-    subscribe: function(param, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, 'ParsePushNotification', 'subscribe',[{
+    	subscribe: function(param, successCallback, errorCallback) {
+        	cordova.exec(successCallback, errorCallback, 'ParsePushNotification', 'subscribe',[{
 			'channel': param.channel
 		}]);
-    }
+    	},
     
-    getInstallationId: function(successCallback, errorCallback) {
-        cordova.exec(
-            successCallback,
-            errorCallback,
-            'ParsePushNotification',
-            'getInstallationId',
-            []
-        );
-    },
+    	getInstallationId: function(successCallback, errorCallback) {
+	        cordova.exec(successCallback, errorCallback, 'ParsePushNotification', 'getInstallationId',[]);
+    	},
 
-    getInstallationObjectId: function(successCallback, errorCallback) {
-        cordova.exec(
-            successCallback,
-            errorCallback,
-            'ParsePushNotification',
-            'getInstallationObjectId',
-            []
-        );
-    },    
-    
-    
+    	getInstallationObjectId: function(successCallback, errorCallback) {
+        	cordova.exec(successCallback, errorCallback, 'ParsePushNotification', 'getInstallationObjectId', []);
+    	}
 };
 
 module.exports = parsePushNotification;
